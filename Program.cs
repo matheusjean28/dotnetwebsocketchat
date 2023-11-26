ea37112d5480a624e306ea9015148df207d8ed54
matheusjean28 <matheusjean28@gmail.com>
2023-11-26 14:32:13 -0300

    start with simple text

--- Program.cs
+++ Program.cs
@@ -1,6 +1,64 @@
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var webSockets = new ConcurrentBag<WebSocket>();
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseWebSockets();
+
+app.Use(async (context, next) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        
+        webSockets.Add(webSocket);
+
+        await HandleWebSocketAsync(webSocket);
+    }
+    else
+    {
+        await next();
+    }
+});
+
+async Task HandleWebSocketAsync(WebSocket webSocket)
+{
+    var buffer = new byte[1024 * 4];
+    var cancellationToken = new CancellationToken();
+
+    while (webSocket.State == WebSocketState.Open)
+    {
+        try
+        {
+            var result = await webSocket.ReceiveAsync(buffer, cancellationToken);
+
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                var message = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                foreach (var socket in webSockets)
+                {
+                    if (socket.State == WebSocketState.Open)
+                    {
+                        await socket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, true, cancellationToken);
+                    }
+                }
+            }
+            else if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, cancellationToken);
+            }
+        }
+        catch (WebSocketException)
+        {
+            break;
+        }
+    }
+}
 
 app.Run();
